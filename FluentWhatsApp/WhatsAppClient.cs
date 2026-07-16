a4b3b1dd304d83dbdc5d1fd0d972d6c933e91566
Siraj M <sirajmsaha@gmail.com>
2026-07-16 12:47:44 +0400

    chore: add `System.Text.Json` to `WhatsAppClient`

--- FluentWhatsApp/WhatsAppClient.cs
+++ FluentWhatsApp/WhatsAppClient.cs
@@ -1,5 +1,6 @@
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Text.Json;
 using FluentWhatsApp.Exceptions;
 using FluentWhatsApp.Message.Models;
 using FluentWhatsApp.Message.Requests;
